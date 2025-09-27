bfd461008067f5fa646187c6416ee7aeef436a2e
tahfeezmizan <mizanahmed5316@gmail.com>
2025-09-28 03:33:59 +0600

    apply job

---
+++ src/redux/features/application.ts
@@ -0,0 +1,26 @@
+import { baseApi } from "./baseApi";
+
+const applicationApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    applyJob: builder.mutation({
+      query: (data) => ({
+        url: `/application`,
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Application"],
+    }),
+    getApplication: builder.query({
+      query: () => ({
+        url: "/application",
+        method: "GET",
+      }),
+      providesTags: ["Application"],
+      //   transformResponse: (response: any) => {
+      //     return response?.data?.data;
+      //   },
+    }),
+  }),
+});
+
+export const { useApplyJobMutation, useGetApplicationQuery } = applicationApi;
--- src/types/types.ts
+++ src/types/types.ts
@@ -108,3 +108,12 @@ export type JobData = {
   jobType: string;
   applications: number;
 };
+
+export type JobApplyFormInputs = {
+  name: string;
+  email: string;
+  phone: string;
+  location: string;
+  experience: number;
+  resume: FileList;
+};
