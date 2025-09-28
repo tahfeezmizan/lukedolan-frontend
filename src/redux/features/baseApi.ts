72068b63386624a3573522f4d0c235cb6c501a5e
apponislam <11appon11@gmail.com>
2025-09-29 04:33:14 +0600

    dashboard fixed

--- src/redux/features/baseApi.ts
+++ src/redux/features/baseApi.ts
@@ -18,7 +18,7 @@ export const baseApi = createApi({
             return headers;
         },
     }),
-    tagTypes: ["Auth", "Product", "Jobs", "Chat", "Message", "Application", "Category", "Plan"],
+    tagTypes: ["Auth", "Product", "Jobs", "Chat", "Message", "Application", "Category", "Plan", "DashboardStatistics"],
     endpoints: (builder) => ({
         getProfile: builder.query<any, void>({
             query: () => "/profile",
