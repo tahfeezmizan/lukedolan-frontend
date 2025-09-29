71fe7be5b39c6bf21d05ab80b82d1636adbe5d1a
apponislam <11appon11@gmail.com>
2025-09-29 06:02:29 +0600

    hireing hero section created 2

--- src/components/common/home/hiring-hero.tsx
+++ src/components/common/home/hiring-hero.tsx
@@ -5,13 +5,14 @@ import hiringImg from "@/assets/hiring.png";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { useGetAllUserQuery } from "@/redux/features/userApi";
+import CountUp from "@/lib/CountUp";
 
 export default function HiringHero() {
     const { data, isLoading } = useGetAllUserQuery({});
 
     const activeUsers = data?.data?.staticData?.totalUsers || 0;
     const applicationsSubmitted = data?.data?.staticData?.totalApplicants || 0;
-
+    // hii
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 py-20">
             <div className="flex flex-col md:flex-row items-center justify-center gap-10 md:gap-20">
@@ -25,11 +26,11 @@ export default function HiringHero() {
 
                     <div className="flex items-center gap-20 mb-8">
                         <div className="space-y-1">
-                            <h3 className="text-xl font-bold text-gray-400">{isLoading ? "---" : activeUsers.toLocaleString()}+</h3>
+                            <h3 className="text-3xl font-bold text-gray-400">{isLoading ? "---" : <CountUp end={activeUsers} />}+</h3>
                             <p className="text-sm">Active Users</p>
                         </div>
                         <div className="space-y-1">
-                            <h3 className="text-xl font-bold text-gray-400">{isLoading ? "---" : applicationsSubmitted.toLocaleString()}+</h3>
+                            <h3 className="text-3xl font-bold text-gray-400">{isLoading ? "---" : <CountUp end={applicationsSubmitted} />}+</h3>
                             <p className="text-sm">Job Applications Submitted</p>
                         </div>
                     </div>
